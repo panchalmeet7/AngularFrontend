e3b74eec4da94103ffc05327c578d217cae52e60
meet <meetpanchal194@gmail.com>
2023-07-25 11:56:39 +0530

    push: login page ui

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
 
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  type:string = "password";
+  isText : boolean = false;
+  eyeIcon :string = "fa-eye-slash"
+
+  hideshowPass(){
+  this.isText = !this.isText;
+  this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash"; //if its text then show eye icon
+  this.isText ? this.type = "text" : this.type = "password"; //if its eye then type text 
+  }
 }
