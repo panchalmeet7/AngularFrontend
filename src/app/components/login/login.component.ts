f1d301b623cfec2794d3cc05b1ea0d0c5ab92c47
meet <meetpanchal194@gmail.com>
2023-07-25 14:14:21 +0530

    push : sign in and sign up page ui

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {NgIf} from '@angular/common';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login',
@@ -12,19 +9,22 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 export class LoginComponent {
   email = new FormControl('', [Validators.required, Validators.email]);
 
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
   type:string = "password";
   isText : boolean = false;
   eyeIcon :string = "fa-eye-slash"
+   
+  constructor () {}
 
   hideshowPass(){
-  this.isText = !this.isText;
-  this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash"; //if its text then show eye icon
-  this.isText ? this.type = "text" : this.type = "password"; //if its eye then type text 
+  this.isText = !this.isText; // make it true and false 
+  this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash"; //if its text then show open eye icon
+  this.isText ? this.type = "text" : this.type = "password"; //if its open eye icon then type text 
   }
+
+  // getErrorMessage() {
+  //   if (this.email.hasError('required')) {
+  //     return 'You must enter a value';
+  //   }
+  //   return this.email.hasError('email') ? 'Not a valid email' : '';
+  // }
 }
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
+
+  constructor (){}
+  
+  ngOnInit(): void {
+    console.log("Signup component initialized");
+  }
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  type:string = "password";
+  isText : boolean = false;
+  eyeIcon :string = "fa-eye-slash"
+   
+  hideshowPass(){
+  this.isText = !this.isText; // make it true and false 
+  this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash"; //if its text then show open eye icon
+  this.isText ? this.type = "text" : this.type = "password"; //if its open eye icon then type text 
+  }
+
+  // getErrorMessage() {
+  //   if (this.email.hasError('required')) {
+  //     return 'You must enter a value';
+  //   }
+  //   return this.email.hasError('email') ? 'Not a valid email' : '';
+  // }
 
 }
